df852b8d168cb59cd0e9e2ee82f1b71b7890be91
Galih Chyo <ccahyogalih@gmail.com>
2025-05-21 18:20:47 +0700

    Add files via upload

---
+++ home/Latihan Java/ketiga/isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>"); 
+document.writeln("<HR WIDTH=600 COLOR=Black>"); 
+document.writeln("<H1>Cahyo Galih Wisang Jati</H1>"); 
+document.writeln("<H2>Pakintelan Ke.Gunung Pati Kota Semarang<H2>"); 
+document.writeln("<H3>+62 812-3261-7752<H3>"); 
+document.writeln("<HR WIDTH=600 COLOR=Black>"); 
+document.writeln("</CENTER>");
